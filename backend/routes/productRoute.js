42f9847d1823b6b775b03e68c76dedfa57879932
Von <vonlagareofficial@gmail.com>
2020-07-06 21:34:35 +0800

    Pagination RestmAPI version1

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -22,7 +22,41 @@ router.get("/", async (req, res) => {
   const products = await Product.find({ ...category, ...searchKeyword }).sort(
     sortOrder
   );
-  res.send(products);
+  res.json(products);
+});
+
+router.get("/test", async (req, res) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const result = {};
+
+  if (endIndex < (await Product.find({}).countDocuments().exec())) {
+    result.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    result.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+
+  try {
+    // Find all products and limit to the given limit then skip base on start index
+    result.result = await Product.find({}).limit(limit).skip(startIndex).exec();
+
+    const paginatedResult = result;
+    res.status(200).json(paginatedResult);
+  } catch (error) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 router.post("/addproduct", async (req, res) => {
